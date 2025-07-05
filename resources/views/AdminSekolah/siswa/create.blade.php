1e2c46a336a0e032d979e45637193c259743ab0d
Amelia311 <ameliarezqi122@gmail.com>
2025-07-05 08:12:11 +0700

    amel

--- resources/views/AdminSekolah/siswa/create.blade.php
+++ resources/views/AdminSekolah/siswa/create.blade.php
@@ -6,86 +6,41 @@
 <link rel="stylesheet" href="{{ asset('css/AdminSekolah/style_create.css') }}">
 @endpush
 
-@section('content')
-<<<<<<< HEAD
-
-<div class="content-box">
-  <h3 class="box-title">Tambah Siswa</h3>
+@section ('content')
+<div class="form-wrapper">
   <form action="{{ route('siswa.store') }}" method="POST" class="form-siswa">
     @csrf
     <div class="form-grid">
+      <h3>Tambah Siswa</h3>
+      <br>
       <div class="form-group">
         <label for="nama">Nama Siswa</label>
-        <input type="text" name="nama" id="nama" required>
+        <input type="text" name="nama" required>
       </div>
       <div class="form-group">
         <label for="nisn">NISN</label>
-        <input type="text" name="nisn" id="nisn" required>
+        <input type="text" name="nisn" required>
       </div>
       <div class="form-group">
         <label for="asal_sekolah">Asal Sekolah</label>
-        <input type="text" name="asal_sekolah" id="asal_sekolah" required>
+        <input type="text" name="asal_sekolah" required>
       </div>
       <div class="form-group">
         <label for="alamat">Alamat</label>
-        <textarea name="alamat" id="alamat" required></textarea>
+        <textarea name="alamat" required></textarea>
       </div>
-      <div class="form-group" style="grid-column: span 2;">
+      <div class="form-group">
         <label for="kelas">Kelas</label>
-        <select name="kelas" id="kelas" required>
+        <select name="kelas" required>
           <option value="">Pilih Kelas</option>
           <option value="X">X</option>
           <option value="XI">XI</option>
           <option value="XII">XII</option>
         </select>
       </div>
-=======
-<div class="content-box">
-
-  <form action="{{ route('siswa.store') }}" method="POST" class="form-siswa">
-    @csrf
-
-    <h3>Tambah Siswa</h3>
-
-    <div class="form-group">
-      <label for="nama">Nama Siswa</label>
-      <input type="text" name="nama" id="nama" required>
     </div>
-
-    <div class="form-group">
-      <label for="nisn">NISN</label>
-      <input type="text" name="nisn" id="nisn" required>
-    </div>
-
-    <div class="form-group">
-      <label for="asal_sekolah">Asal Sekolah</label>
-      <input type="text" name="asal_sekolah" id="asal_sekolah" required>
-    </div>
-
-    <div class="form-group">
-      <label for="alamat">Alamat</label>
-      <textarea name="alamat" id="alamat" required></textarea>
-    </div>
-
-    <div class="form-group">
-      <label for="kelas">Kelas:</label>
-      <select name="kelas" id="kelas" required>
-        <option value="">Pilih Kelas</option>
-        <option value="X">X</option>
-        <option value="XI">XI</option>
-        <option value="XII">XII</option>
-      </select>
->>>>>>> 87ee3aff6b295eb98e50a9d640630156eab3c8e0
-    </div>
-
-    <button type="submit" class="btn-submit">Simpan</button>
+    <button type="submit" class="btn-tambah">Simpan</button>
   </form>
-<<<<<<< HEAD
-</div>
 
 
-=======
-
-</div>
->>>>>>> 87ee3aff6b295eb98e50a9d640630156eab3c8e0
 @endsection
